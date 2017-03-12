3d78134e37374e27fe9f2723a8d6cbffaabf2dfc
sh4ne <shane.andrus@aggiemail.usu.edu>
2017-03-12 11:50:01 -0600

    pushing everything to repo

--- Scala/src/Benchmarks.scala
+++ Scala/src/Benchmarks.scala
@@ -31,6 +31,22 @@ object Benchmarks {
     case _ => fib(i - 1) + fib(i - 2)
   }
 
+  /** Dijkstra's Algorithm - Code taken from https://rosettacode.org/wiki/Dijkstra%27s_algorithm#Scala */
+
+  type Path[Key] = (Double, List[Key])
+
+    def Dijkstra[Key](lookup: Map[Key, List[(Double, Key)]], fringe: List[Path[Key]], dest: Key, visited: Set[Key]): Path[Key] = fringe match {
+      case (dist, path) :: fringe_rest => path match {case key :: path_rest =>
+        if (key == dest) (dist, path.reverse)
+        else {
+          val paths = lookup(key).flatMap {case (d, key) => if (!visited.contains(key)) List((dist + d, key :: path)) else Nil}
+          val sorted_fringe = (paths ++ fringe_rest).sortWith {case ((d1, _), (d2, _)) => d1 < d2}
+          Dijkstra(lookup, sorted_fringe, dest, visited + key)
+        }
+      }
+      case Nil => (0, List())
+    }
+
   def readFileToList(file: String): List[Int] =  {
     val lines = fromFile(file).getLines
     var list = new ListBuffer[Int]()
@@ -48,17 +64,31 @@ object Benchmarks {
   }
 
   def measure[T](block: => T): T = {
-    val started = System.currentTimeMillis
+    val started = System.currentTimeMillis()
     val res = block
     logPerf(System.currentTimeMillis - started)
     res
   }
 
     def main(args: Array[String]): Unit = {
-      val fibPer = measure(fib(45))
+      measure(fib(45))
 
       var filepath = "C:\\Users\\Shane\\Documents\\CS4700-Benchmarking\\Benchmarking\\array.txt"
       val list = readFileToList(filepath)
-      measure(mergeSort(list))
+      val sorted = measure(mergeSort(list))
+
+      /** from small graph */
+      val lookup = Map(
+        "a" -> List((3.0, "c"), (4.0, "e")),
+        "b" -> List((2.0, "c"), (1.0, "d"), (1.0,"e")),
+        "c" -> List((3.0, "a"), (2.0, "b"),(3.0,"e")),
+        "d" -> List((1.0, "b"), (1.0,"e")),
+        "e" -> List((4.0, "a"), (1.0, "b"), (3.0,"c"),(1.0,"d")),
+        "f" -> Nil
+      )
+      val res = Dijkstra[String](lookup, List((0, List("a"))), "e", Set())
+      measure(Dijkstra[String](lookup, List((0, List("a"))),"e",Set()))
+      println(res)
+
     }
 }
